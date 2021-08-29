5a007df5f1747f3977d2719d2f09bb5720518740
Pranav <pranavrajnath@gmail.com>
2021-08-30 03:41:27 +0530

    seven commit

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["pranavraj.herokuapp.com","localhost"]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nonstatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
 ROOT_URLCONF = 'ChatApp.urls'
 
 TEMPLATES = [
@@ -123,5 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
